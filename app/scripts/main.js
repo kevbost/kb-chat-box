61989329a25a4c6b34b1061cbe48fbd901073350
Kevin Bost <kevbost@gmail.com>
2014-06-01 12:50:58 -0400

    everything still works, added moment js, looking into removing moment and rendering epoch time into moment instead.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
+// sets user default
 var user = 'KB';
+// sets momentJS default
+var now = moment().calendar();
 
 // ///////////////////////////////////////////////
 // ///////////////////////////////////// Templates
@@ -11,8 +14,10 @@ var chatBox = _.template($('.chatBoxTemplate').text());
 // ///////////////////////////////////////////////
 // ///////////////////////////////////// Fetch API
 // ///////////////////////////////////////////////
+// server url variable
 var serverURL = 'http://tiny-pizza-server.herokuapp.com/collections/chat-messages';
-var json = $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(chatApi) {
+// getJSON, saved in the json variable
+$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(chatApi) {
 	renderChat(chatApi);
 });
 
@@ -24,6 +29,7 @@ function renderChat(data){
 	data.forEach(function(info){
 		if(info.user && info.time && info.message) {
 		var rendered = chatBox(info);
+		_.last( rendered, [1])
 		$('.main').prepend(rendered);
 	}});
 }
@@ -33,6 +39,7 @@ function Message(user, message, time) {
 	this.message = message || 'Empty message?!';
 	this.time = time || 'Immediately.';
 	this.meta = 'very m374';
+	this.appID = 'kevbost';
 }
 
 function update(info) {
@@ -41,12 +48,18 @@ function update(info) {
 
 $('button').click(function(){
 	var message = $('.textfield2').val();
-	var time = Date.now();
+	var time = now;
 	var postMessage = new Message(user, message, time);
 	update(postMessage);
 	$.get('http://tiny-pizza-server.herokuapp.com/collections/chat-messages')
 	alert("success");
+	location.reload();
 });
+
+
+
+$( ".main" ).scrollTop( 300 )
+
 
 // (function() {
 // 	setInterval(function() {
