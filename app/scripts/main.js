6ebb48c8d44244b3dddc10dbc437ef4ddfbb9053
Kevin Bost <kevbost@gmail.com>
2014-06-01 22:17:40 -0400

    scroll to bottom now works like a charm.  username input still has no effect on post

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,18 @@
 'use strict';
 
-// sets user default
 var user = 'KB';
+
+// User Login
+$('.submit-username').click(function(){
+	var user = $('.input-username').val();
+	$('.identity, .main').addClass("activate");
+	alert('Yo whatsup ' + user + '?');
+	$('.container').scrollTop($('.main')[0].scrollHeight);
+});
+
 // sets momentJS default
 var now = Date.now();
 // var now = moment().calendar();
-
 
 // ///////////////////////////////////////////////
 // ///////////////////////////////////// Templates
@@ -16,19 +23,20 @@ var chatBox = _.template($('.chatBoxTemplate').text());
 // ///////////////////////////////////////////////
 // ///////////////////////////////////// Fetch API
 // ///////////////////////////////////////////////
-// server url variable
-var serverURL = 'http://tiny-pizza-server.herokuapp.com/collections/chat-messages';
-// getJSON, saved in the json variable
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(chatApi) {
+// getJASON
+function loadChat () {
+	// $('.main').html('');
+	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(chatApi) {
 	renderChat(chatApi);
 });
+}
 
 
 // ///////////////////////////////////////////////
 // ///////////////////////////////Render functions
 // ///////////////////////////////////////////////
 function renderChat(data){
-	data = data.splice(0,25);
+	data = data.splice(0,50);
 	data.forEach(function(info){
 		if(info.user && info.time && info.message) {
 		var rendered = chatBox(info);
@@ -39,7 +47,7 @@ function renderChat(data){
 
 function Message(user, message, time) {
 	this.user = user || 'KB';
-	this.message = message || 'Empty message?!';
+	this.message = message || 'KB just submitted an empty message from his local chat client.';
 	this.time = time || 'Immediately.';
 	this.meta = 'very m374';
 	this.appID = 'kevbost';
@@ -49,15 +57,32 @@ function update(info) {
 		$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
 }
 
-$('button').click(function(){
+$('.submit-message').click(function(){
 	var message = $('.textfield2').val();
 	var time = now;
 	var postMessage = new Message(user, message, time);
 	update(postMessage);
-	$.get('http://tiny-pizza-server.herokuapp.com/collections/chat-messages')
 	alert("success");
-	location.reload();
 });
+
+// setTimeout(function() {
+//       loadChat();
+// }, 5000);
+
+loadChat();
+
+// setInterval(loadChat,1000);
+
+
+
+// RELOAD Works but doesnt refresh from json
+// var auto_refresh = setInterval(function () {
+//     $('.container').fadeOut('slow', function() {
+//         $(this).load('/some/url', function() {
+//             $(this).fadeIn('slow');
+//         });
+//     });
+// }, 1000); // refresh every 15000 milliseconds
 
 // 2:08 PM DATE
 // yeah, it's similar to setInterval, but instead of re-running it every X milliseconds, 
