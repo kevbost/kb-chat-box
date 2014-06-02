62adb1a9177d11fdd71506c0bed4fa4a38e0289c
Kevin Bost <kevbost@gmail.com>
2014-06-02 00:01:14 -0400

    completed final touches, restyled elements and cleaned up code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,22 @@
 'use strict';
 
-var user = 'KB';
+var user = $('.input-username').val();
 
 // User Login
 $('.submit-username').click(function(){
 	var user = $('.input-username').val();
 	$('.identity, .main').addClass("activate");
-	alert('Yo whatsup ' + user + '?');
+	// alert('Yo whatsup ' + user + '?');
 	$('.container').scrollTop($('.main')[0].scrollHeight);
 });
 
 // sets momentJS default
 var now = Date.now();
-// var now = moment().calendar();
 
-// ///////////////////////////////////////////////
-// ///////////////////////////////////// Templates
-// ///////////////////////////////////////////////
+// //////////////////////////////////////////////////// Template
 var chatBox = _.template($('.chatBoxTemplate').text());
 
-
-// ///////////////////////////////////////////////
-// ///////////////////////////////////// Fetch API
-// ///////////////////////////////////////////////
-// getJASON
+// //////////////////////////////////////////////////// getJASON
 function loadChat () {
 	// $('.main').html('');
 	$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/chat-messages').done(function(chatApi) {
@@ -31,10 +24,7 @@ function loadChat () {
 });
 }
 
-
-// ///////////////////////////////////////////////
-// ///////////////////////////////Render functions
-// ///////////////////////////////////////////////
+// ////////////////////////////////////////////// Render Function
 function renderChat(data){
 	data = data.splice(0,50);
 	data.forEach(function(info){
@@ -45,6 +35,7 @@ function renderChat(data){
 	}});
 }
 
+// /////////////////////////////////////////// Message Constructor
 function Message(user, message, time) {
 	this.user = user || 'KB';
 	this.message = message || 'KB just submitted an empty message from his local chat client.';
@@ -53,11 +44,14 @@ function Message(user, message, time) {
 	this.appID = 'kevbost';
 }
 
+// ///////////////////////////////////////////////// Post function
 function update(info) {
 		$.post('http://tiny-pizza-server.herokuapp.com/collections/chat-messages', info);
 }
 
+// //////////////////////////////////// Click to run post function
 $('.submit-message').click(function(){
+	var user
 	var message = $('.textfield2').val();
 	var time = now;
 	var postMessage = new Message(user, message, time);
@@ -65,42 +59,6 @@ $('.submit-message').click(function(){
 	alert("success");
 });
 
-// setTimeout(function() {
-//       loadChat();
-// }, 5000);
-
 loadChat();
 
 // setInterval(loadChat,1000);
-
-
-
-// RELOAD Works but doesnt refresh from json
-// var auto_refresh = setInterval(function () {
-//     $('.container').fadeOut('slow', function() {
-//         $(this).load('/some/url', function() {
-//             $(this).fadeIn('slow');
-//         });
-//     });
-// }, 1000); // refresh every 15000 milliseconds
-
-// 2:08 PM DATE
-// yeah, it's similar to setInterval, but instead of re-running it every X milliseconds, 
-// it waits that time instead and then runs it....note it's not a silver bullet though for 
-// getting things to happen at certain times. You'll have to remember that JS is 
-// asynchronous and will keep looking for shit to do. I.e. if you put a setTimeout 
-// into a for/forEach loop the loop will likely be completed before your set timeouts even start
-
-
-// $( ".main" ).scrollTop( 300 )
-
-
-// (function() {
-// 	setInterval(function() {
-// 		$.getJSON(serverURL).done(function(chatApi) {
-// 			renderChat(chatApi);
-// 		});
-// 	}, 3000);
-// })();
-
-// $('.main').append(chatBox);
